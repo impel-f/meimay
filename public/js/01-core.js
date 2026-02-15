d2a798b8be5637bb5ae401dcb36899c097ed7a19
impel-f <8mitsu.burst@gmail.com>
2026-02-15 14:22:35 +0900

    Fix critical syntax error in 01-core.js (orphaned code block)

--- public/js/01-core.js
+++ public/js/01-core.js
@@ -109,11 +109,12 @@ function toSeion(str) {
         .replace(/[ばびぶべぼ]/g, s => String.fromCharCode(s.charCodeAt(0) - 1))
         .replace(/[ぱぴぷぺぽ]/g, s => String.fromCharCode(s.charCodeAt(0) - 2));
 }
-const found = master.find(k => k['漢字'] === c);
-return {
-    kanji: c,
-    strokes: found ? (parseInt(found['画数']) || 0) : 0
-};
+surnameData = chars.map(c => {
+    const found = master.find(k => k['漢字'] === c);
+    return {
+        kanji: c,
+        strokes: found ? (parseInt(found['画数']) || 0) : 0
+    };
 });
 
 console.log("CORE: Surname data updated ->", surnameData);
